814e89e169bdf3bc882f13439fac71af542cc2ba
Anne017 <anneonyme017@netcourrier.com>
2019-05-12 21:27:13 +0200

    French translation added

---
+++ translations/isodrive-fr.ts
@@ -0,0 +1,57 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr">
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../qml/main.qml" line="20"/>
+        <source>none</source>
+        <translation>aucune</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="21"/>
+        <source>Active: %1</source>
+        <translation>Active : %1</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="28"/>
+        <source>Eject</source>
+        <translation>Ejecter</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="35"/>
+        <source>Refresh</source>
+        <translation>Actualiser</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="45"/>
+        <source>Authentication required</source>
+        <translation>Authentification nécessaire</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="46"/>
+        <source>Please enter your user PIN or password to continue:</source>
+        <translation>Veuillez saisir votre code PIN ou votre mot de passe pour continuer :</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="49"/>
+        <source>PIN or password</source>
+        <translation>Code PIN ou mot de passe</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="54"/>
+        <source>Ok</source>
+        <translation>Valider</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="68"/>
+        <source>Cancel</source>
+        <translation>Annuler</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="121"/>
+        <source>No .iso files found in the &apos;Downloads&apos; folder. Download a hybrid ISO file to continue.</source>
+        <translation>Aucun fichier .iso trouvé dans le dossier « Téléchargements ». Téléchargez un fichier hybride .iso pour continuer.</translation>
+    </message>
+</context>
+</TS>
