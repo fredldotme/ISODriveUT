1c28a5f30eaa077ebbcca1078f5bff48d1cfd4c9
JanConGitHub <80941905+JanConGitHub@users.noreply.github.com>
2021-03-27 07:07:08 +0100

    Create isodrive-nb.ts
    Norwegian translation

---
+++ translations/isodrive-nb.ts
@@ -0,0 +1,73 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../qml/main.qml" line="19"/>
+        <source>none</source>
+        <translation>ingen</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="24"/>
+        <source>ISODrive</source>
+        <translation type="unfinished">ISODrive</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="26"/>
+        <source>Active: %1</source>
+        <translation>Aktiv: %1</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="30"/>
+        <source>Eject</source>
+        <translation>Løs ut</translation>
+    </message>
+    <message>
+        <source>Refresh</source>
+        <translation type="vanished">Oppfrisk</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="39"/>
+        <location filename="../qml/main.qml" line="112"/>
+        <source>Settings</source>
+        <translation>Innstillinger</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="61"/>
+        <source>Authentication required</source>
+        <translation>Autentisering kreves</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="62"/>
+        <source>Please enter your user PIN or password to continue:</source>
+        <translation>Vennligst skriv inn PIN-koden eller passordet for å fortsette:</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="73"/>
+        <source>PIN or password</source>
+        <translation>PIN eller passord</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="79"/>
+        <location filename="../qml/main.qml" line="124"/>
+        <source>Ok</source>
+        <translation>Ok</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="94"/>
+        <source>Cancel</source>
+        <translation>Avbryt</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="116"/>
+        <source>Enable MTP while using ISODrive</source>
+        <translation>Aktiver MTP mens du bruker ISODrive</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="207"/>
+        <source>No .iso files found in the &apos;Downloads&apos; folder. Download a hybrid ISO file to continue.</source>
+        <translation>Ingen .iso-filer ble funnet i & apos;Nedlastinger&apos;mappen. Last ned en hybrid ISO-fil for å fortsette.</translation>
+    </message>
+</context>
+</TS>
